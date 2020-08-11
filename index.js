6a6403c3820f7f245e07dd332eed4c115a117c17
Amanda Claproth <amanda.claproth@gmail.com>
2020-08-11 08:52:46 -0700

    handleClick

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-/*----- constants -----*/
+// /*----- constants -----*/
 
-class Cards {
-    constructor(cardFront, cardBack, cardValue) {
-        this.cardFront = cardFront;
-        this.cardBack = cardBack;
-        this.cardValue = cardValue;
-    }
+const restartDiv = document.querySelector('button');
+const cardsEl = document.querySelectorAll('.card');
+
+
+for (const cards of cardsEl) {
+    cards.addEventListener('click', handleClick)
 };
-let card1 = new Cards("imgUrl('photos/gbusters.jpg')", "imgUrl('photos/blackrect.png')", 1);
-let card2 = new Cards("imgUrl('photos/batman.jpg')", "imgUrl('photos/blackrect.png')", 2);
-let card3 = new Cards("imgUrl('photos/bbhead.jpg')", "imgUrl('photos/blackrect.png')", 3);
-let card4 = new Cards("imgUrl('photos/bjuice.jpg')", "imgUrl('photos/blackrect.png')", 4);
-let card5 = new Cards("imgUrl('photos/bworld.jpg')", "imgUrl('photos/blackrect.png')", 5);
-let card6 = new Cards("imgUrl('photos/gbumps.jpg')", "imgUrl('photos/blackrect.png')", 6);
-let card7 = new Cards("imgUrl('photos/halone.jpg')", "imgUrl('photos/blackrect.png')", 7);
-let card8 = new Cards("imgUrl('photos/pokemon.jpg')", "imgUrl('photos/blackrect.png')", 8);
-let card9 = new Cards("imgUrl('photos/prangers.jpg')", "imgUrl('photos/blackrect.png')", 9);
-let card10 = new Cards("imgUrl('photos/sfighter.jpg')", "imgUrl('photos/blackrect.png')", 10);
-let card11 = new Cards("imgUrl('photos/sonic.jpg')", "imgUrl('photos/blackrect.png')", 11);
-let card12 = new Cards("imgUrl('photos/zelda.jpg')", "imgUrl('photos/blackrect.png')", 12);
-/*----- app's state (variables) -----*/
+
+
+function handleClick(evt) {
+    const card = evt.target.id;
+    chosenCards.push(card);
+}
+
+
+
+
+// handleStart();
+// restartDiv.addEventListener('click', handleStart);
+// const handleStart = () => {
+//     restartTime();
+//     chosenCards = [];
+//     totalCardsWon = [];
+// };
+
+
+// // /*----- app's state (variables) -----*/
 let chosenCards = [];
 let totalCardsWon = [];
-/*----- cached element references -----*/
-const restartDiv = document.querySelector('.button');
-/*----- event listeners -----*/
+// /*----- cached element references -----*/
 
-addEventListener('click', handleFlipCard);
-addEventListener('click', handleRestart);
-/*----- functions -----*/
-init();
-shuffle();
-checkTimer();
-render();
-compareCard();
-updateScore();
-flipCardBack();
-gameOver();
+// /*----- event listeners -----*/
+
+//flipCard.addEventListener('click', handleFlipCard);
+
+// /*----- functions -----*/
+// init();
+// shuffle();
+// checkTimer();
+// render();
+// compareCard();
+// updateScore();
+// flipCardBack();
+// gameOver();
